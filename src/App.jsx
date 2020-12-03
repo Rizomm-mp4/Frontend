dcbe2eb91039e2dd233ccf98af13a0b6543c673c
MTLantoine <mortelier.antoine@hotmail.fr>
2020-12-04 00:51:44 +0100

    🐛 fix nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import './App.css';
-import Auth from "./component/Auth"
+import React from "react";
+import "./App.css";
+import Auth from "./component/Auth";
+import Nav from "./Nav";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>App works !</h1>
-      <Auth />
-    </div>
+    <Router>
+      <Nav />
+      <div className="App">
+        <h1>App works !</h1>
+        <Route path="/login" component={Auth} />
+      </div>
+    </Router>
   );
 }
 
