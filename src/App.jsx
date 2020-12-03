6804bd634778cad9cc5a6aba1d3fa774ff42e3dc
MTLantoine <mortelier.antoine@hotmail.fr>
2020-12-04 00:30:59 +0100

    🎨 add route

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Auth from "./component/Auth"
 
 function App() {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
   return (
     <div className="App">
-      <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          Page has been open for <code>{count}</code> seconds.
-        </div>
-      </header>
+      <h1>App works !</h1>
+      <Auth />
     </div>
   );
 }
---
+++ src/component/Auth.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Auth() {
+  return (
+    <div className="Auth">
+        <h1>Auth works !</h1>
+    </div>
+  );
+}
+
+export default Auth;
