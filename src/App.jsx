2182eaaed45ab01a1b53fdc927c55d946b5e950d
MTLantoine <mortelier.antoine@hotmail.fr>
2020-12-04 02:47:34 +0100

    🔥 remove dope css

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import "./App.css";
-import "./assets/css/material-dashboard-rtl.css";
-import "./assets/css/material-dashboard.min.css";
-import "./assets/css/material-dashboard.css";
 
 import Auth from "./component/Auth";
-import Nav from "./component/Nav";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-      <Nav />
       <div className="App">
         <h1>App works !</h1>
         <Route path="/login" component={Auth} />
