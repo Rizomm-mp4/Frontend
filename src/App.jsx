b89a44015099150ec8a63b4091a84d11999225ae
Logan <logan@xps.home>
2020-12-04 01:10:00 +0100

    ✅ nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "./App.css";
+import "./assets/css/material-dashboard-rtl.css";
+import "./assets/css/material-dashboard.min.css";
+import "./assets/css/material-dashboard.css";
+
 import Auth from "./component/Auth";
-import Nav from "./Nav";
+import Nav from "./component/Nav";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
